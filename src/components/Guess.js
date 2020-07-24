ff136403b8a90bbf0a86be7cf01cabcbfc81062b
Agata <akowalska@juniper.net>
2020-07-24 12:54:09 +0200

    added logic to Guess component

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactGA from 'react-ga';
 
 
 export function Guess(props) {
 
     const [value, setValue] = useState('');
-    const [url, setUrl] = useState('/react/level/1/');
+    const [url, setUrl] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -15,23 +15,35 @@ export function Guess(props) {
             category: 'Form Submit',
             action: 'Save the answer'
         })
+
+        const link = `/react/level/${value}/`;
+
+        setUrl(link);
+
     }
 
     return (
         <>
             <h1> Zagadka nr <b> {props.match.params.level} </b></h1>
             <p> Tekst zagadki </p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <h2> Rozwiązanie </h2>
 
             <form onSubmit={handleSubmit}>
                 <input type="text" value={value} onChange={(event) => setValue(event.target.value)} placeholder="Enter your solution" />
-                <button> Go ! </button>
+                <button> Sprawdź ! </button>
             </form>
 
 
-            <ReactGA.OutboundLink eventLabel='click-view' to={url} >
-                      Idź do następnej zagadki
-            </ReactGA.OutboundLink>
+            {url && url !== '' &&  <ReactGA.OutboundLink eventLabel='click-view' to={url} >
+                      Następna zagadka
+            </ReactGA.OutboundLink> }
 
         </>
     )
